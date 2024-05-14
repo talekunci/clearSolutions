3a2a833d94f4bdb822b83ff3cefd9096db11555f
talekunci <smouke.king1@gmail.com>
2024-05-15 01:58:09 +0300

    Added sql script

---
+++ src/main/resources/db/migration/V1.0.1__More_Users.sql
@@ -0,0 +1,3 @@
+INSERT INTO users(email, firstname, lastname, birthdate) VALUES ('somebody@email.com', 'Some', 'Body', '1948-06-07');
+INSERT INTO users(email, firstname, lastname, birthdate) VALUES ('somebody1@email.com', 'Some', 'Body', '2001-06-07');
+INSERT INTO users(email, firstname, lastname, birthdate) VALUES ('somebody2@email.com', 'Some', 'Body', '2002-06-07');
