615b474d2c54c4bdd6b32e9e735742f5955ef437
unknown <smouke.king1@gmail.com>
2024-05-21 01:19:14 +0300

    extra words has been deleted from the ExceptionHandlerTests class declaration

--- src/test/java/clear/solutions/assignment/controllers/handlers/ExceptionHandlerTests.java
+++ src/test/java/clear/solutions/assignment/controllers/handlers/ExceptionHandlerTests.java
@@ -27,7 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-public class git statusExceptionHandlerTests {
+public class ExceptionHandlerTests {
 
     private MockMvc mockMvc;
 
